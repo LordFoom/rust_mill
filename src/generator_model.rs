613aafc24a7b2891328d08ba00481035d8ebb2b2
LordFoom <lordfoom@gmail.com>
2025-11-13 21:09:07 +0200

    Exploratory programming Wondering at how to do this best

--- src/generator_model.rs
+++ src/generator_model.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+///I think this should be a type
 pub enum GeneratorType {
     ///Single constant value
     Constant,
@@ -20,14 +23,20 @@ pub struct ColumnGenerator {
     generator_type: GeneratorType,
     length: usize,
     available_values: Option<Vec<String>>,
+    //meta tags used to possibly change or generate
+    meta_values: Option<Vec<String>>,
 }
+
 pub struct Column {
     name: String,
     generator: ColumnGenerator,
+    value: Option<String>,
 }
 
+///The actual data model...do we need this? should be not just keep generating?
+///TOIDO think about this a bit more
 pub struct DataModel {
     //what are the names of our csv columns, eg name, surname, telephone, email, customer_code
-    column_names: Vec<String>,
-    column_valus: Vec,
+    columns: Vec<String>
+    rows: Vec<String>,
 }
